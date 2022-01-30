26c3ec73db40358ecded8de75038fcff0a1b94d8
Takunda <84584653+takundatk23@users.noreply.github.com>
2022-01-30 23:33:40 +0200

    Add files via upload

---
+++ transactions.js
@@ -0,0 +1,25 @@
+const Transactions = [
+    {
+    NFTName: 'Cryptopunk #1111',
+    NFTCollection: 'Cryptopunk',
+    DatePurchased: '19/01/2020',
+    Price: '$2.2million',
+    UniqueBlockchainCode: '#1111'    
+    },
+
+    {
+    NFTName: 'Cryptopunk #1101',
+    NFTCollection: 'Cryptopunk',
+    DatePurchased: '10/06/2019',
+    Price: '$3.0million',
+    UniqueBlockchainCode: '#1101'    
+    },
+
+    {
+    NFTName: 'Cryptopunk #1000',
+    NFTCollection: 'Cryptopunk',
+    DatePurchased: '29/05/2019',
+    Price: '$1.6million',
+    UniqueBlockchainCode: '#1000'    
+    }
+]
